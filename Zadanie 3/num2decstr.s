2e90e5a690785bbda91433a874714c4be451771a
Melee-Cat <81462823+Melee-Cat@users.noreply.github.com>
2022-03-09 20:13:51 +0300

    first

--- Zadanie 3/num2decstr.s
+++ Zadanie 3/num2decstr.s
@@ -4,7 +4,7 @@ num2decstr:
 mov ebx, 10
 mov edx, 0
 div ebx
-add edx, 30h
-mov [rdi+rcx-1], dl
-loop num2decstr
+add edx, 30h; выводим на экран
+mov [rdi+rcx-1], dl; записываем цифру в конец строки 
+loop num2decstr; повторяем пока >0
 ret
